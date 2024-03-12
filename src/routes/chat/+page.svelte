ca4c2d286ae0aa46121f0a2d240fb133f3f9bfb3
AishwaryaSubash <riya129ash@gmail.com>
2024-03-12 20:05:31 +0530

    to akash

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -45,7 +45,7 @@
 	</div> -->
 
 	<div class="h-full w-full flex flex-col">
-		{#each Array(40) as _, index (index)}
+		{#each chats as chat, index (index)}
 			<div class={'flex w-full p-2 ' + (index % 2 === 0 ? 'justify-end ' : 'justify-start ')}>
 				<div
 					class={'flex flex-col w-fit max-w-96 p-2 gap-3 rounded-xl ' +
@@ -55,29 +55,15 @@
 					style="max-width: {maxWidth}px;"
 				>
 					<div class="text-xl font-bold">
-						{#if index % 2 === 0}
+						<!-- {#if index % 2 === 0}
 							<p class="">User</p>
 						{:else}
 							<p class="">SVCE AI</p>
-						{/if}
+						{/if} -->
+						<p class="">{chat.role}</p>
 					</div>
 					<p class={'text-lg ' + (index % 2 === 0 ? 'text-right' : 'text-left')}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet rhoncus est.
-						Curabitur a massa pulvinar, interdum nibh non, sodales turpis. Suspendisse pharetra
-						hendrerit efficitur. Mauris in enim elit. Cras purus magna, pulvinar et leo id,
-						elementum scelerisque ipsum. Nullam ut blandit sem, a auctor ante. Ut aliquam ultrices
-						mollis. In quis ligula ipsum. Vestibulum ultrices cursus nibh non accumsan. Aenean ac
-						pulvinar ipsum, sed lobortis lectus. Donec elementum sapien et malesuada rhoncus.
-						Curabitur tempus lacinia enim, a venenatis urna elementum ut. Donec condimentum dolor
-						nec accumsan fringilla. Proin molestie id nisi ut vehicula. Nunc tincidunt dapibus
-						maximus. Mauris porta convallis sem id euismod. Praesent vehicula ut lorem nec
-						scelerisque. Morbi quis ex et turpis consectetur efficitur. Ut ac felis faucibus,
-						ullamcorper metus vitae, auctor erat. Duis sed tellus sit amet mi scelerisque
-						condimentum vitae ut dui. Pellentesque pharetra nunc lectus, eget maximus orci hendrerit
-						at. Vestibulum pellentesque magna eu eros imperdiet, eget iaculis velit feugiat.
-						Interdum et malesuada fames ac ante ipsum primis in faucibus. In ultricies posuere sem
-						eget varius. Fusce at cursus ante, sed pellentesque enim. Duis eros massa, blandit nec
-						volutpat eu, facilisis vel enim.
+						{chat.message}
 					</p>
 				</div>
 			</div>
